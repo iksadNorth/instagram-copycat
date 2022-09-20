a9c74a614aa3af7c2b6935b8ea5ac10be4b848d3
iksadNorth <iksadnorth@gmail.com>
2022-09-20 18:15:19 +0900

    #4 - 도메인 설계 * 자바 클래스로 도메인 표현해보기 1. 공통 엔티티를 추출. 2. 위 사항을 위해 JpaConfig를 설정.

---
+++ backend/src/main/java/me/iksadnorth/insta/model/entity/BaseEntity.java
@@ -0,0 +1,42 @@
+package me.iksadnorth.insta.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Where(clause = "deleted_at is NULL")
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
