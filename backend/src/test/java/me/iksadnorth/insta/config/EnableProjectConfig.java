583ae3839064019b7ac6490bd5fb582c7cac801c
iksadNorth <iksadnorth@gmail.com>
2022-09-29 12:05:39 +0900

    #6 - Account 관련 문제 해결 * 단위 Test 위한 설정 작업들. 1. AccountFixture를 만들어 Test에 활용토록 함. 2. static 함수도 test 하기 위해 build.gradle에 추가적인 의존성을 기입.

---
+++ backend/src/test/java/me/iksadnorth/insta/config/EnableProjectConfig.java
@@ -0,0 +1,16 @@
+package me.iksadnorth.insta.config;
+
+import me.iksadnorth.insta.config.properies.JwtProperties;
+import me.iksadnorth.insta.service.AccountService;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Import({AuthenticationConfig.class, JwtProperties.class, AccountService.class})
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnableProjectConfig {
+}
