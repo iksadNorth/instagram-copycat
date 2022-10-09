2f308ca78447cd6c597db48908a77dd90b22af96
iksadNorth <iksadnorth@gmail.com>
2022-10-09 21:13:42 +0900

    #10 - 우측에 3개의 행(row)를 만들기

--- frontend/src/views/view_home.vue
+++ frontend/src/views/view_home.vue
@@ -1,19 +1,21 @@
 <template>
-<v-container><v-row justify="center">
+<v-container><v-row id="body">
     <!-- 좌측 열 -->
     <v-col id="left--sector" 
-    justify="center"
-    cols="3"
-    ><v-row justify="center">
+    cols="5"
+    ><v-row>
         <v-img
         src="@/assets/homepageLeft.png"
         ></v-img>
     </v-row></v-col>
+
     <!-- 우측 열 -->
     <v-col id="right--sector"
-    cols="3"
+    cols="5"
     >
-
+        <v-row class="right--rows"><com-login class="w-100"/></v-row>
+        <v-row class="right--rows"><com-join class="w-100"/></v-row>
+        <v-row class="right--rows"><com-app class="w-100"/></v-row>
     </v-col>
 </v-row></v-container>
 </template>
@@ -34,14 +36,29 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
     #left--sector {
         background-color: beige;
     }
     #right--sector {
         background-color: rgb(156, 203, 153);
     }
+    #body {
+        background-color: blueviolet;
+    }
+    .right--rows {
+        background-color: aqua;
+        
+        margin: 1px;
+    }
+    .v-row {
+        justify-content: center;
+        align-items: center;
+    }
     .v-col {
         padding: 30px;
+
+        justify-content: center;
+        align-items: center;
     }
 </style>
