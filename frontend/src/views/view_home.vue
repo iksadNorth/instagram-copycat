c91af62f21da90e816726e2debdbc8ccc7526d39
iksadNorth <iksadnorth@gmail.com>
2022-10-11 13:32:35 +0900

    #12 - 좌측 feed 창과 우측의 팔로우 추천 창, 자신의 계정 정보 창 공간 확보.

--- frontend/src/views/view_home.vue
+++ frontend/src/views/view_home.vue
@@ -1,28 +1,63 @@
 <template>
-<v-container><v-row id="body">
-    <!-- 좌측 열 -->
-    <v-col id="left--sector" 
-    class="pa-16"
-    >
-        <v-img
-        src="@/assets/homepageLeft.png"
-        ></v-img>
-    </v-col>
+<!-- 로그인하지 않았을 때 -->
+<v-container v-if="!isLogin">
+    <v-row id="body">
+        <!-- 좌측 열 -->
+        <v-col id="left--sector" 
+        class="pa-16"
+        >
+            <v-img
+            src="@/assets/homepageLeft.png"
+            ></v-img>
+        </v-col>
 
-    <!-- 우측 열 -->
-    <v-col id="right--sector"
-    cols="5"
-    >
-        <v-row class="right--rows"><com-login class="w-100"/></v-row>
-        <v-row class="right--rows"><com-join class="w-100"/></v-row>
-        <v-row class="right--rows"><com-app class="w-100"/></v-row>
-    </v-col>
-</v-row></v-container>
+        <!-- 우측 열 -->
+        <v-col id="right--sector"
+        cols="5"
+        >
+            <v-row class="right--rows"><com-login class="w-100"/></v-row>
+            <v-row class="right--rows"><com-join class="w-100"/></v-row>
+            <v-row class="right--rows"><com-app class="w-100"/></v-row>
+        </v-col>
+    </v-row>
+</v-container>
+
+<!-- 로그인했을 때 -->
+<v-container v-if="isLogin">
+    <v-row id="body" align="start">
+        <!-- 좌측 열 -->
+        <v-col id="left--sector" 
+        >
+            <com-article-sm :data="data"/>
+            <com-article-sm :data="data"/>
+        </v-col>
+
+        <!-- 우측 열 -->
+        <v-col id="right--sector"
+        >
+            <com-profile :data="data" size="60"/>
+            <com-follow-rec :data="data"/>
+        </v-col>
+    </v-row>
+</v-container>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            isLogin: true,
 
+            data: {
+                uid: 634643,
+                writer: "kakao_career",
+                imgSrc: "article-img-example.jpg",
+                likes: 83,
+                content: "모델 구조가 스케일링에 어떠한 영향을 미칠까?",
+                createdAt: "8월 5",
+            },
+        }
+    }
 }
 </script>
 
