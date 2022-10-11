568c70bd4476c15771806f1310147709f184fb88
iksadNorth <iksadnorth@gmail.com>
2022-10-12 01:57:43 +0900

    #13 - explore 페이지 생성.

--- frontend/src/route/index.js
+++ frontend/src/route/index.js
@@ -3,6 +3,7 @@ import { createWebHistory, createRouter } from 'vue-router';
 const routes = [
   { path: '/', component: () => import("@/views/view_home.vue")},
   { path: '/accounts/emailsignup', component: () => import("@/views/view_emailsignup.vue")},
+  { path: '/explore', component: () => import("@/views/view_explore.vue")},
 ];
 
 export default createRouter({
