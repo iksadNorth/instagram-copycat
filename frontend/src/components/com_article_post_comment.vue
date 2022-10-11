8f24abd27ae3c3561ca5d3c7f7a182244af0e4c0
iksadNorth <iksadnorth@gmail.com>
2022-10-11 13:33:44 +0900

    #12 - 좌측 feed 창 제작.

---
+++ frontend/src/components/com_article_post_comment.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="d-flex flex-row align-center">
+        <v-text-field
+            :label="inputs.comment.label"
+            variant="plain"
+        ></v-text-field>
+        <p 
+        @click="onClickPostComment" class="clickable text-blue"
+        ><strong>{{ btn.comment.label }}</strong></p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            inputs: {
+                comment: {label: "댓글 달기..."},
+            },
+            btn: {
+                comment: {label: "게시"},
+            },
+        }
+    },
+    methods: {
+        onClickPostComment() {console.log("Click onClickPostComment")},
+    },
+}
+</script>
+
+<style scoped>
+    .clickable {
+        cursor: pointer;
+    }
+</style>
