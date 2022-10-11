fbe94ce9491216068ecc2932ed3ce313a0805583
iksadNorth <iksadnorth@gmail.com>
2022-10-11 13:34:13 +0900

    #12 - 전역 컴포넌트 선언.

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,11 +1,20 @@
-import login from "@/components/com_login";
-import join from "@/components/com_join";
-import app_download from "@/components/com_app_download";
-import joinform from "@/components/com_joinform";
-import birthform from "@/components/com_birthform";
-import vertificationform from "@/components/com_vertificationform";
-import termform from "@/components/com_termform";
-import loginquery from "@/components/com_loginquery";
+import login                    from "@/components/com_login";
+import join                     from "@/components/com_join";
+import app_download             from "@/components/com_app_download";
+import joinform                 from "@/components/com_joinform";
+import birthform                from "@/components/com_birthform";
+import vertificationform        from "@/components/com_vertificationform";
+import termform                 from "@/components/com_termform";
+import loginquery               from "@/components/com_loginquery";
+import article_small            from "@/components/com_article_small";
+import article_content          from "@/components/com_article_content";
+import article_post_comment     from "@/components/com_article_post_comment";
+import article_likes            from "@/components/com_article_likes";
+import article_created_at       from "@/components/com_article_created_at";
+import article_profile          from "@/components/com_article_profile";
+import article_tools            from '@/components/com_article_tools'
+import image                    from '@/components/com_image'
+import follow_recommend         from '@/components/com_follow_recommend'
 
 export default {
     install(Vue) {
@@ -17,6 +26,17 @@ export default {
         Vue.component("com-vertificationform", vertificationform);
         Vue.component("com-termform", termform);
         Vue.component("com-loginquery", loginquery);
+        Vue.component("com-article-sm", article_small);
+
+        Vue.component("com-article-content", article_content);
+        Vue.component("com-post-comment", article_post_comment);
+        Vue.component("com-likes", article_likes);
+        Vue.component("com-createdAt", article_created_at);
+        Vue.component("com-profile", article_profile);
+        Vue.component("com-article-tools", article_tools);
+        Vue.component("com-img", image);
+
+        Vue.component("com-follow-rec", follow_recommend);
     }
 };
   
