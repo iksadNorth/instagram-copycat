98d6ea9f0eaed63268fd8cb8e3540576a02f3f47
iksadNorth <iksadnorth@gmail.com>
2022-10-13 00:52:22 +0900

    #15 - 특정 계정 페이지 넣기

--- frontend/src/route/index.js
+++ frontend/src/route/index.js
@@ -5,6 +5,7 @@ const routes = [
   { path: '/accounts/emailsignup', component: () => import("@/views/view_emailsignup.vue")},
   { path: '/explore', component: () => import("@/views/view_explore.vue")},
   { path: '/p/:pid', component: () => import("@/views/view_article.vue")},
+  { path: '/:uid', component: () => import("@/views/view_account.vue")},
 ];
 
 export default createRouter({
---
+++ frontend/src/views/view_account.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-container>
+        <v-row>
+            <com-account-profile class="mx-16 my-6" :data="user" />
+        </v-row>
+        <v-divider class="my-6" />
+        <v-row>
+            <com-container-img :album="album" />
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                uid: 634643,
+                name: "iksad",
+                nickname: "kakao_career",
+                articles: 832,
+                followers: 2535,
+                followings: 13,
+                introduction: "자기소개 하는 란.",
+            },
+            album: [
+                {
+                    pid: 90384,
+                    imgSrc: "https://img.freepik.com/premium-photo/abstract-blue-background-with-smooth-lines_476363-5999.jpg?w=900",
+                    likes: 42,
+                    comments: 21,
+                },
+                {
+                    pid: 66367,
+                    imgSrc: "https://img.freepik.com/premium-photo/city-landscape-with-group-building-sky-sunlight_254791-864.jpg?w=900",
+                    likes: 15,
+                    comments: 14,
+                },
+                {
+                    pid: 8734,
+                    imgSrc: "https://img.freepik.com/premium-photo/background-empty-red-dark-podium-with-lights-tile-floor-3d-rendering_314485-400.jpg?w=900",
+                    likes: 72,
+                    comments: 16,
+                },
+                {
+                    pid: 34987,
+                    imgSrc: "https://img.freepik.com/free-photo/water-texture-background-pastel-blue-design_53876-146663.jpg?w=900&t=st=1665476193~exp=1665476793~hmac=42a4b7f81eee85146011a5ca0d710b4c063080bc440e7f2c3ec9ecf205f4ff83",
+                    likes: 36,
+                    comments: 8,
+                },
+            ],
+        }
+    },
+    computed: {
+        uid() {return this.$route.params.uid;}
+    },
+}
+</script>
+
+<style>
+
+</style>
